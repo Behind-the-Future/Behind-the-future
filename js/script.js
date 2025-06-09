9821adaf1376a9f0a42ee7668bda65f2916b832a
frodrigues03 <femoreira2021@gmail.com>
2025-06-08 21:55:17 -0300

    add if else js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,28 @@ function validarFormulario() {
   let erros = [];
 
   
+  if (!nome) {
+    erros.push("O campo Nome é obrigatório.");
+    document.getElementById('nome').style.borderColor = 'red';
+  } else if (nome.length < 3) {
+    erros.push("O nome deve conter pelo menos 3 caracteres.");
+    document.getElementById('nome').style.borderColor = 'red';
+  }
+
+  
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email) {
+    erros.push("O campo E-mail é obrigatório.");
+    document.getElementById('email').style.borderColor = 'red';
+  } else if (!emailRegex.test(email)) {
+    erros.push("O e-mail fornecido não é válido.");
+    document.getElementById('email').style.borderColor = 'red';
+  }
+
+  if (!mensagem) {
+    erros.push("O campo Mensagem é obrigatório.");
+    document.getElementById('mensagem').style.borderColor = 'red';
+  } else if (mensagem.length < 10) {
+    erros.push("A mensagem deve conter pelo menos 10 caracteres.");
+    document.getElementById('mensagem').style.borderColor = 'red';
+  }
